54f0bd2859badce2a2143eff7c51203de58b1389
LuisMejiasR <luisme.1993@gmail.com>
2024-12-27 09:56:24 -0300

    feat: adding get endpoint

---
+++ src/main/java/med/voll/api/direccion/Direccion.java
@@ -0,0 +1,60 @@
+package med.voll.api.direccion;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public @Data class Direccion {
+    private String calle;
+    private String numero;
+    private String distrito;
+    private String complemento;
+
+    public Direccion() {
+    }
+
+    public Direccion(DatosDireccion datosDireccion) {
+        this.calle = datosDireccion.calle();
+        this.numero = datosDireccion.numero();
+        this.complemento = datosDireccion.complemento();
+        this.distrito = datosDireccion.distrito();
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getDistrito() {
+        return distrito;
+    }
+
+    public void setDistrito(String distrito) {
+        this.distrito = distrito;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+}
